ad4e8f6dc48f2555319cbf61d51ec2b44b1fb36c
Camilovelez1 <camilo95022@gmail.com>
2024-04-20 10:44:08 -0500

    add lstm

--- taller_volatilidad/redes_neuronales/RNN_USD_AUD/rnn_model.py
+++ taller_volatilidad/redes_neuronales/RNN_USD_AUD/rnn_model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class RNNModel(nn.Module):
     def __init__(self, input_size, hidden_size, output_size,dropout):
         super(RNNModel, self).__init__()
-        self.rnn = nn.LSTM(input_size, hidden_size, dropout, batch_first=True)
+        self.rnn = nn.LSTM(input_size, hidden_size, dropout=dropout, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
--- taller_volatilidad/redes_neuronales/RNN_USD_CAD/rnn_model.py
+++ taller_volatilidad/redes_neuronales/RNN_USD_CAD/rnn_model.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 
 
 class RNNModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size,dropout):
         super(RNNModel, self).__init__()
-        self.rnn = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.rnn = nn.LSTM(input_size, hidden_size, dropout=dropout, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
